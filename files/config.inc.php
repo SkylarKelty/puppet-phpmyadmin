1995c0d4aecd07ed3cbd614484d8e263c481b2fc
Skylar Kelty <skylarkelty@gmail.com>
2014-01-18 14:22:03 +0000

    Initial Commit
    Add config and the basic installation scripts

---
+++ files/config.inc.php
@@ -0,0 +1,80 @@
+<?php
+/* vim: set expandtab sw=4 ts=4 sts=4: */
+/**
+ * phpMyAdmin sample configuration, you can use it as base for
+ * manual configuration. For easier setup you can use setup/
+ *
+ * All directives are explained in documentation in the doc/ folder
+ * or at <http://docs.phpmyadmin.net/>.
+ *
+ * @package PhpMyAdmin
+ */
+
+/*
+ * This is needed for cookie based authentication to encrypt password in
+ * cookie
+ */
+$cfg['blowfish_secret'] = '8yc8uh9mhyc2m'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+
+/*
+ * Servers configuration
+ */
+$i = 0;
+
+/*
+ * First server
+ */
+$i++;
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['compress'] = false;
+/* Select mysql if your server does not have mysqli */
+$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+/*
+ * phpMyAdmin configuration storage settings.
+ */
+
+/* User used to manipulate with storage */
+$cfg['Servers'][$i]['controlhost'] = '127.0.0.1';
+$cfg['Servers'][$i]['controluser'] = 'pma';
+$cfg['Servers'][$i]['controlpass'] = '';
+
+/* Storage database and tables */
+$cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
+$cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
+$cfg['Servers'][$i]['relation'] = 'pma__relation';
+$cfg['Servers'][$i]['table_info'] = 'pma__table_info';
+$cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
+$cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
+$cfg['Servers'][$i]['column_info'] = 'pma__column_info';
+$cfg['Servers'][$i]['history'] = 'pma__history';
+$cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
+$cfg['Servers'][$i]['tracking'] = 'pma__tracking';
+$cfg['Servers'][$i]['designer_coords'] = 'pma__designer_coords';
+$cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
+$cfg['Servers'][$i]['recent'] = 'pma__recent';
+$cfg['Servers'][$i]['users'] = 'pma__users';
+$cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
+$cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
+
+/*
+ * End of servers configuration
+ */
+
+/*
+ * Directories for saving/loading files from server
+ */
+$cfg['UploadDir'] = '/tmp/phpMyAdmin';
+$cfg['SaveDir'] = '/tmp/phpMyAdmin';
+
+
+/*
+ * Sky - I got annoyed a lot and this doesnt need to be secure or performant
+ */
+$cfg['LoginCookieValidity'] = 360000;
+$cfg['MaxRows'] = 100;
---
+++ manifests/init.pp
@@ -0,0 +1,49 @@
+class phpmyadmin {
+   exec {
+      'install-phpmyadmin':
+         path    => '/bin/:/usr/bin',
+         command => 'curl -SsL https://github.com/phpmyadmin/phpmyadmin/archive/STABLE.tar.gz | tar xzfv - && mv -i phpmyadmin-STABLE /var/www/html/phpMyAdmin',
+         creates => '/var/www/html/phpMyAdmin',
+         cwd     => '/tmp';
+      'install-phpmyadmin-db':
+         path    => '/bin/:/usr/bin',
+         command => 'cat /var/www/html/phpMyAdmin/examples/create_tables.sql | mysql -u root',
+         unless  => 'test `mysql phpmyadmin -NBe \'SHOW TABLES\' | wc -l` -gt 0',
+         require => [ Exec['install-phpmyadmin'], Mysql_database['phpmyadmin'] ];
+   }
+
+   file {
+      '/var/www/html/phpMyAdmin/config.inc.php':
+         ensure  => present,
+         source  => "puppet:///modules/phpmyadmin/config.inc.php",
+         owner   => 'root',
+         group   => 'root',
+         mode    => 0644,
+         require => Exec['install-phpmyadmin'];
+   }
+
+   mysql_database {
+      'phpmyadmin':
+         ensure   => 'present',
+         charset  => 'utf8',
+         collate  => 'utf8_bin',
+         require  => [Package['mysql_client', 'mysql-server'], Service['mysqld']];
+   }
+
+   mysql_user {
+      'pma@127.0.0.1':
+         ensure   => 'present',
+         password => '',
+         require  => [Package['mysql_client', 'mysql-server'], Service['mysqld']];
+   }
+
+   mysql_grant {
+      'pma@127.0.0.1/phpmyadmin.*':
+         ensure     => 'present',
+         options    => ['GRANT'],
+         privileges => ['ALL'],
+         table      => 'phpmyadmin.*',
+         user       => 'pma@127.0.0.1',
+         require    => [Package['mysql_client', 'mysql-server'], Service['mysqld']];
+   }
+}
